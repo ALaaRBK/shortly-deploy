47c7fff409734bb66d5a8b18aa1bd002ea884237
AlaaRBK <alshyoukhi94@gmail.com>
2017-09-22 12:58:39 +0300

    final modify

--- app/models/user.js
+++ app/models/user.js
@@ -5,43 +5,34 @@ var mongoose = require('mongoose')
 
   var username= mongoose.Schema({
     
-    username : {type : string, index).unique();
+    username : {type : string, index{unique :true }}, 
     user.string('password', 100);
 
   })
+var User = mongoose.module('User',userSchema);
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
+User.comparePassword.prototype = function(attemptedPassword, callback) {
     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
+      if (err) { 
+        callback(err)
+      }else{
+      callback(null,isMatch);
+    }
+    })
+  };
+
+
+userSchema.pre('save',function() {
     var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
+    return cipher(this.password, null, null).bind(this)
       .then(function(hash) {
-        this.set('password', hash);
+        this.password =  hash;
+        next();
       });
-  }
-});
+    })
+
+
 
 module.exports = User;
 
 
-//       link.increments('id').primary();
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
